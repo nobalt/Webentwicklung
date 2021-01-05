7d5229fa25d2a58f08af3ba09734aecceb2ba6ea
Nora <s4nobalt@uni-trier.de>
2021-01-05 21:32:42 +0100

    Übung 5

--- Übung1-4/Navigation.php
+++ Übung1-4/Navigation.php
@@ -5,8 +5,8 @@
                     <div class="col">
                         <ul class="list-group">
                             <li class="list-group-item"><a href="Login.php">Login</a></li>
-                            <li class="list-group-item"><a href="Projekte.php">Projekte</a></li>
-                            <li class="list-group-item"><a href="Aktuelle_Projekte.php">Aktuelle Projekte</a></li>
+                            <li class="list-group-item"><a href="../../../../Übung1-4/Projekte.php">Projekte</a></li>
+                            <li class="list-group-item"><a href="../../../../Übung1-4/Aktuelle_Projekte.php">Aktuelle Projekte</a></li>
                         </ul>
                     </div>
                 </div>
@@ -14,7 +14,7 @@
                     <div class="col-1"></div>
                     <div class="col">
                         <ul class="list-group">
-                            <li class="list-group-item"><a href="Reiter.php">Reiter</a></li>
+                            <li class="list-group-item"><a href="../../../../Übung1-4/Reiter.php">Reiter</a></li>
                             <li class="list-group-item"><a href="Aufgaben.php">Aufgaben</a></li>
                             <li class="list-group-item"><a href="Personen.php">Personen</a></li>
                         </ul>
